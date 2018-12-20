916ecd1237fccad099454f3dd02c30038a702d8a
Daffa4040 <alifdaffahernanda@gmail.com>
2018-12-21 04:37:16 +0800

    Create Payment.php

---
+++ Projects/Controller/Payment.php
@@ -0,0 +1,17 @@
+<?php
+class Payment {
+	private $typecard;
+	private $numbercard;
+    private $expiredate;
+    private $cvc;
+    private $name;
+
+function settypecard($typecard){
+	$this->typecard=$typecards;
+}
+
+function setnumbercard($numbercard){
+	$this->typecard=$typecards;
+}
+    
+}
